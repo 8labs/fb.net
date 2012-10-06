f0676f91d5f8c8130d7b06ca752fe78e4c4a9f08
Kellen Piffner <k@8labs.com>
2012-10-06 09:03:09 -0500

    letting VS correct some line spacing.

--- fb.net/fb.net/Facebook.cs
+++ fb.net/fb.net/Facebook.cs
@@ -8,67 +8,67 @@ using System.Collections.Generic;
 
 namespace fb.net
 {
-	public class Facebook
-	{
+    public class Facebook
+    {
 
-		private RestClient restClient;
-		private string token;
+        private RestClient restClient;
+        private string token;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="fb.net.Facebook"/> class.
-		/// </summary>
-		/// <param name='access_token'>
-		/// Facebook access token.
-		/// </param>
-		public Facebook (string access_token)
-		{
-			this.token = access_token;
-			this.restClient = new RestClient("https://graph.facebook.com");
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="fb.net.Facebook"/> class.
+        /// </summary>
+        /// <param name='access_token'>
+        /// Facebook access token.
+        /// </param>
+        public Facebook(string access_token)
+        {
+            this.token = access_token;
+            this.restClient = new RestClient("https://graph.facebook.com");
+        }
 
-		public FacebookUser GetUser()
-		{
-			FacebookUser me = ApiCall<FacebookUser>("me", Method.GET);
-			return me;
-		}
+        public FacebookUser GetUser()
+        {
+            FacebookUser me = ApiCall<FacebookUser>("me", Method.GET);
+            return me;
+        }
 
 
-		public T ApiCall<T>(String method, Method httpType)
-		{
-			return ApiCall<T>(method, httpType, null);
-		}
+        public T ApiCall<T>(String method, Method httpType)
+        {
+            return ApiCall<T>(method, httpType, null);
+        }
 
-		public T ApiCall<T>(String method, Method httpType, KeyValuePair<string, string>[] args)
+        public T ApiCall<T>(String method, Method httpType, KeyValuePair<string, string>[] args)
         {
             return (T)ApiCall(typeof(T), method, httpType, args);
         }
 
-		public object ApiCall(Type type, String method, Method httpType, KeyValuePair<string, string>[] args)
-		{
-			var request = new RestRequest(method, httpType);
-			request.AddParameter("access_token", token);
+        public object ApiCall(Type type, String method, Method httpType, KeyValuePair<string, string>[] args)
+        {
+            var request = new RestRequest(method, httpType);
+            request.AddParameter("access_token", token);
 
-			if (args != null)
-			{
-				foreach (KeyValuePair<string, string> arg in args)
-				{
-					request.AddParameter(arg.Key, arg.Value);
-				}
-			}
+            if (args != null)
+            {
+                foreach (KeyValuePair<string, string> arg in args)
+                {
+                    request.AddParameter(arg.Key, arg.Value);
+                }
+            }
 
-			IRestResponse response = restClient.Execute(request);
+            IRestResponse response = restClient.Execute(request);
 
             if (response.ErrorException != null)
             {
                 throw response.ErrorException;
             }
 
-			object obj = JsonConvert.DeserializeObject(response.Content, type);
+            object obj = JsonConvert.DeserializeObject(response.Content, type);
 
-			return obj;
-		}
+            return obj;
+        }
 
 
-	}
+    }
 }
 
